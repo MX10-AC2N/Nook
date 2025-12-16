a0db335c734949b870662d8eec9eebf6d2c6b0d7
MX10-AC2N <62643837+MX10-AC2N@users.noreply.github.com>
2025-12-16 19:03:54 +0100

    Update +layout.svelte

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,9 +1,9 @@
 <script>
-  import '../lib/ui/themes/jardin.css';
-  import '../lib/ui/themes/space.css';
-  import '../lib/ui/themes/maison.css';
+  import '$lib/ui/themes/jardin.css';
+  import '$lib/ui/themes/space.css';
+  import '$lib/ui/themes/maison.css';
   import { onMount } from 'svelte';
-  import { currentTheme } from '../lib/ui/ThemeStore';
+  import { currentTheme } from '$lib/ui/ThemeStore';
 
   onMount(() => {
     currentTheme.subscribe(theme => {
