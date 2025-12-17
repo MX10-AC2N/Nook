983851c57258d61c11930d6b7fb30b9a25fa8e1d
MX10-AC2N <62643837+MX10-AC2N@users.noreply.github.com>
2025-12-17 14:36:36 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,5 +7,15 @@ export default defineConfig({
     port: 5173,
     strictPort: true,
     host: true
+  },
+  ssr: {
+    noExternal: [
+      'tsparticles',
+      '@tsparticles/slim',
+      '@tsparticles/engine',
+      '@tsparticles/svelte',
+      '@tsparticles/shape-star' // Au cas où, pour le shape star que tu utilises
+    ]
+    // Cette option force Vite à bundler ces libs client-only au lieu de les externaliser pendant le SSR
   }
 });
