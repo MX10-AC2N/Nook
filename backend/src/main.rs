bed39ee59f6278b37107b73167dcea153c28a687
MX10-AC2N <62643837+MX10-AC2N@users.noreply.github.com>
2025-12-17 07:49:56 +0100

    Update main.rs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -58,8 +58,8 @@ async fn main() {
         .route("/api/webrtc/offer", post(webrtc::handle_offer))
         .route("/api/webrtc/answer", get(webrtc::handle_answer))
         .route("/ws", get(ws_handler))
-        .nest_service("/static", ServeDir::new("/app/static"))
         .nest_service("/uploads", ServeDir::new("/app/data/uploads"))
+        .nest_service("/", ServeDir::new("/app/static"))
         .fallback_service(ServeFile::new("/app/static/index.html"))
         .with_state(shared_state);
 
