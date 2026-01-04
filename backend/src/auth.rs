d027b08ed21582b5146d2d01162f2fcbbdc47163
MX10-AC2N <62643837+MX10-AC2N@users.noreply.github.com>
2026-01-04 15:46:59 +0100

    Update auth.rs

--- backend/src/auth.rs
+++ backend/src/auth.rs
@@ -58,7 +58,7 @@ pub struct AuthResponse {
 
 // Utilitaires
 
-fn hash_password(password: &str) -> String {
+pub fn hash_password(password: &str) -> String {
     let salt = SaltString::generate(&mut OsRng);
     let argon2 = Argon2::default();
     argon2.hash_password(password.as_bytes(), &salt).unwrap().to_string()
