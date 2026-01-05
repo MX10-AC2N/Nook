d155494a4dd86b621a06fea665e11394b72c4290
MX10-AC2N <62643837+MX10-AC2N@users.noreply.github.com>
2026-01-05 17:15:30 +0100

    Update db.rs

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -10,6 +10,8 @@ use serde::{Deserialize, Serialize};
 use std::sync::Arc;
 use uuid::Uuid;
 
+use crate::SharedState;
+
 // === STRUCTURES DE DONNÉES ===
 
 #[derive(Clone, Debug, Serialize, Deserialize, sqlx::FromRow)]
