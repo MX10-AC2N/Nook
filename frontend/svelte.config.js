5e317935cbe0d52a76f749892ea2219d71e2adbb
MX10-AC2N <62643837+MX10-AC2N@users.noreply.github.com>
2026-01-09 13:52:21 +0100

    Update svelte.config.js

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -3,34 +3,34 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	compilerOptions: {
-		runes: true,
-		accessors: true,
-		immutable: false
-	},
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		}),
- appDir: 'app',
- files: {
-   appTemplate: 'src/app.html'
- }
-		alias: {
-			$lib: './src/lib',
-			$components: './src/components',
-			$routes: './src/routes',
-			$assets: './static'
-		},
-		serviceWorker: {
-			register: false
-		}
-	}
+  preprocess: vitePreprocess(),
+  compilerOptions: {
+    runes: true,
+    accessors: true,
+    immutable: false
+  },
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false,
+      strict: true
+    }),
+    appDir: 'app',
+    files: {
+      appTemplate: 'src/app.html'
+    },
+    alias: {
+      $lib: './src/lib',
+      $components: './src/components',
+      $routes: './src/routes',
+      $assets: './static'
+    },
+    serviceWorker: {
+      register: false
+    }
+  }
 };
 
 export default config;
