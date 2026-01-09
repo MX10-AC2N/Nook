926b9b04733a5b50ddbe7e8cee89175213236792
MX10-AC2N <62643837+MX10-AC2N@users.noreply.github.com>
2026-01-09 13:28:17 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-       assetsInclude: ['**/*.svg'],
+  assetsInclude: ['**/*.svg'],
 build: {
 		rollupOptions: {
 			external: ['libsodium-wrappers-sumo']
