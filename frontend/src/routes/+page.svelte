d0a6924f82b30bcb123b831facd5c697d90d2cee
MX10-AC2N <62643837+MX10-AC2N@users.noreply.github.com>
2026-01-20 10:28:38 +0100

    Update +page.svelte

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
-	import { isAuthenticated, isAdmin, authLoading } from '$lib/authStore';
+	import { isAuthenticated, isAdmin, authLoading } from '$lib/authStore.svelte';
 	import { onMount } from 'svelte';
 	import { browser } from '$app/environment';
 
